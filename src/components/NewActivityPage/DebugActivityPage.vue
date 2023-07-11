5d42b04a7c0350698ac857622ed8c62ff1395b47
cody-1263 <90823697+cody-1263@users.noreply.github.com>
2023-07-12 02:45:23 +0300

    DebugActivityPage: plain list of raids

--- src/components/NewActivityPage/DebugActivityPage.vue
+++ src/components/NewActivityPage/DebugActivityPage.vue
@@ -5,6 +5,7 @@ import { ref, inject } from 'vue';
 import { ActivityCalculatorNew } from '@/model/ActivityCalculatorNew';
 import LoadingIndicator from '../Common/LoadingIndicator.vue';
 import { HtActivityType, HtPeriodMode } from '@/services/HtAppService';
+import { DestinyClanProfile } from '@/domain/ProfileDataItems';
 
 // services
 const serviceContainer = inject(htKeys.htServiceContainerKey)!;
@@ -14,6 +15,36 @@ const appService = serviceContainer.htAppService;
 
 // const
 const someStringNumbers = ['0','4','8','12','16','20','24'];
+let displayText = ref(['hello world','hello world 222']);
+
+async function onFullReload() {
+  let clan1 = new DestinyClanProfile();
+  clan1.name = "Pathfinders";
+  clan1.groupId = '3909446';
+  clan1.clanCallsign = "SGP";
+  var members = await bnetProvider.getClanMembers(clan1, domain);
+  
+  for (let mem of members) {
+    if (!mem.bungieGlobalDisplayName.includes('cody#1263')) {
+      continue;
+    }
+    
+    var activities = await bnetProvider.getActivities(mem, new Date('2023-06-01T03:24:00'), 4);
+    
+    // objs.sort((a,b) => (a.last_nom > b.last_nom) ? 1 : ((b.last_nom > a.last_nom) ? -1 : 0))
+    
+    activities.sort((a,b) => (a.startDate > b.startDate) ? -1 : 1);
+    
+    let text = new Array<string>();
+    for (let act of activities) {
+      text.push(act.referenceId + '     ' + act.startDate.toString()+ '     '  + act.durationSeconds/60 + 'm     ');
+    }
+    
+    displayText.value = text;
+  }
+  
+  
+}
 
 </script>
 
@@ -22,14 +53,13 @@ const someStringNumbers = ['0','4','8','12','16','20','24'];
   <div style="height: 2rem;"></div>
   <div><img src="@/assets/sticker-01.png"></div>
   <div style="height: 2rem;"></div>
-  <button>_ reload all _</button>
+  <button @click="(ev) => onFullReload()">_ reload all _</button>
   <div style="height: 2rem;"></div>
   
-  <div> my custom text </div>
-  <div> my custom text </div>
-  <div> my custom text </div>
-  <div> my custom text </div>
-  <div> my custom text </div>
+  <div v-for="textItem of displayText">
+    {{textItem}}
+  </div>
+
   
 </template>
 
@@ -39,7 +69,7 @@ const someStringNumbers = ['0','4','8','12','16','20','24'];
 
 
 * {
-  font-family: Arial, Helvetica, sans-serif;
+  font-family: Consolas, Helvetica, sans-serif;
   font-size: 10pt;
   color: #fff9;
 }
